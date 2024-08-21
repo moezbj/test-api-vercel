ecdbd73ac743dda699ce06a723f499b925de1516
moez <moezbj.93@gmail.com>
2024-08-21 11:52:31 +0100

    update types

--- api/index.ts
+++ api/index.ts
@@ -2,33 +2,32 @@ import { ApolloServer, gql } from "apollo-server-express";
 import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import http from "http";
 
-import { port, databaseUri } from '../src/config/vars'
-import prisma from '../src/config/prisma'
-import getApp from '../src/config/express'
+import { port, databaseUri } from "../src/config/vars";
+import prisma from "../src/config/prisma";
+import getApp from "../src/config/express";
 
-import mongoose from 'mongoose'
-import { application } from '../src/modules'
+import mongoose from "mongoose";
+import { application } from "../src/modules";
 
 /* const app = express();
 app.use(cors());
 app.use(express.json()); */
 
-
 const connectDB = async () => {
   try {
     if (databaseUri) {
-      await mongoose.connect(databaseUri)
-      console.log('🎉 connected to database successfully')
+      await mongoose.connect(databaseUri);
+      console.log("🎉 connected to database successfully");
     }
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
-connectDB()
-prisma.$connect()
+};
+connectDB();
+prisma.$connect();
 
-const schema = application.createSchemaForApollo()
-const app = getApp()
+const schema = application.createSchemaForApollo();
+const app = getApp();
 const httpServer = http.createServer(app);
 
 /* const typeDefs = gql`
@@ -43,7 +42,7 @@ const resolvers = {
   },
 }; */
 
-const startApolloServer = async (app, httpServer) => {
+const startApolloServer = async (app: any, httpServer: any) => {
   const server = new ApolloServer({
     schema,
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
