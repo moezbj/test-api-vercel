a621089c2ca4d5fe7071fc61ca46f84f8e46a9a3
moez <moezbj.93@gmail.com>
2024-08-23 11:45:58 +0100

    update

--- api/index.ts
+++ api/index.ts
@@ -1,23 +1,22 @@
-import getServer from '../src/config/apollo'
-import { port, databaseUri } from '../src/config/vars'
-import prisma from '../src/config/prisma'
-import mongoose from 'mongoose'
+import getServer from "../src/config/apollo";
+import { port, databaseUri } from "../src/config/vars";
+import prisma from "../src/config/prisma";
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
     if (databaseUri) {
-      await mongoose.connect(databaseUri)
-      console.log('🎉 connected to database successfully')
+      await mongoose.connect(databaseUri);
+      console.log("🎉 connected to database successfully");
     }
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
-connectDB()
-prisma.$connect()
-
+};
+connectDB();
+prisma.$connect();
 getServer().then((httpServer) => {
   httpServer.listen({ port }, () => {
-    console.log(`🚀 Server ready at http://localhost:${port}/graphql`)
-  })
-})
+    console.log(`🚀 Server ready at http://localhost:${port}/graphql`);
+  });
+});
