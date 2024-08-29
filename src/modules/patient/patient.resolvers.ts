b77cfb50179a4eb5e221ba809f1f2a926687c325
moez <moezbj.93@gmail.com>
2024-08-29 11:21:49 +0100

    add test on user in patient

--- src/modules/patient/patient.resolvers.ts
+++ src/modules/patient/patient.resolvers.ts
@@ -1,66 +1,70 @@
-import { getUser } from '../../middlewares/getUser'
-import prisma from '../../config/prisma'
+import { getUser } from "../../middlewares/getUser";
+import prisma from "../../config/prisma";
 
 export const patientResolver = {
   Query: {
     patients: async (
       parent: Record<string, any>,
       args: Record<string, any>,
-      context: any,
+      context: any
     ) => {
-      const getIdUser = await getUser(context.authorization.split(' ')[1])
-      if (!getIdUser) throw new Error('id not provided')
+      const getIdUser = await getUser(context.authorization.split(" ")[1]);
+      if (!getIdUser) throw new Error("id not provided");
       const existUser = await prisma.user.findFirst({
         where: {
           id: getIdUser.sub?.toString(),
         },
-      })
-      if (!existUser) throw new Error("user dosen't exist")
+      });
+      if (!existUser) throw new Error("user dosen't exist");
 
-      const list = await prisma.patient.findMany()
-      return list
+      const list = await prisma.patient.findMany({
+        where: {
+          userId: existUser.id,
+        },
+      });
+      return list;
     },
     patient: async (
       parent: Record<string, any>,
       args: Record<string, any>,
-      context: any,
+      context: any
     ) => {
-      const getIdUser = await getUser(context.authorization.split(' ')[1])
-      if (!getIdUser) throw new Error('id not provided')
+      const getIdUser = await getUser(context.authorization.split(" ")[1]);
+      if (!getIdUser) throw new Error("id not provided");
       const existUser = await prisma.user.findFirst({
         where: {
           id: getIdUser.sub?.toString(),
         },
-      })
-      if (!existUser) throw new Error("user dosen't exist")
+      });
+      if (!existUser) throw new Error("user dosen't exist");
 
       const workspace = await prisma.patient.findFirst({
         where: {
           id: args.id,
         },
-      })
-      return workspace
+      });
+      return workspace;
     },
   },
   Mutation: {
     createPatient: async (parent: undefined, args: any, context: any) => {
-      const arg = args.input
+      const arg = args.input;
 
-      const getIdUser = await getUser(context.authorization.split(' ')[1])
-      if (!getIdUser) throw new Error('id not provided')
+      const getIdUser = await getUser(context.authorization.split(" ")[1]);
+      if (!getIdUser) throw new Error("id not provided");
       const existUser = await prisma.user.findFirst({
         where: {
           id: getIdUser.sub?.toString(),
         },
-      })
-      if (!existUser) throw new Error("user dosen't exist")
+      });
+      if (!existUser) throw new Error("user dosen't exist");
 
       const existPatient = await prisma.patient.findFirst({
         where: {
           name: arg.name,
         },
-      })
-      if (existPatient) throw new Error('user already exist')
+      });
+      if (existPatient) throw new Error("user already exist");
       const [createPatient] = await prisma.$transaction([
         prisma.patient.create({
           data: {
@@ -70,27 +74,28 @@ export const patientResolver = {
             insurance: arg.insurance,
             phone: arg.phone,
             note: arg.note,
+            userId: existUser.id,
           },
         }),
-      ])
-      return createPatient
+      ]);
+      return createPatient;
     },
     updatePatient: async (parent: undefined, args: any, context: any) => {
-      const getIdUser = await getUser(context.authorization.split(' ')[1])
-      if (!getIdUser) throw new Error('id not provided')
+      const getIdUser = await getUser(context.authorization.split(" ")[1]);
+      if (!getIdUser) throw new Error("id not provided");
       const existUser = await prisma.user.findFirst({
         where: {
           id: getIdUser.sub?.toString(),
         },
-      })
-      if (!existUser) throw new Error("user dosen't exist")
+      });
+      if (!existUser) throw new Error("user dosen't exist");
 
       const existPatient = await prisma.patient.findFirst({
         where: {
           name: args.id,
         },
-      })
-      if (existPatient) throw new Error('patient already exist')
+      });
+      if (existPatient) throw new Error("patient already exist");
 
       const [createPatient] = await prisma.$transaction([
         prisma.patient.update({
@@ -106,8 +111,8 @@ export const patientResolver = {
             note: args.note,
           },
         }),
-      ])
-      return createPatient
+      ]);
+      return createPatient;
     },
   },
-}
+};
